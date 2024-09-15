3abbae6e44f8970b519f1c7ca079b046215f7561
Swydk <sanzhar.kasymzhomart@gmail.com>
2024-09-15 10:39:59 +0500

    vbncv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m!aba9_=tv4u6eq3%=)xuvtdhw^8ls2stj67$6=9um@-5^vtsz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['viking62.ru', 'localhost']
 
 
 # Application definition
@@ -133,10 +133,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/static/'
+
+# Настройка медиа файлов
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/app/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
